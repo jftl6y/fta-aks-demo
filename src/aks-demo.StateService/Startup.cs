4f45e3b313ab5066f118604ed1947c0c32ea5e3e
Steve St Jean <steve@sstjean.com>
2020-03-02 14:30:27 -0500

    Updated App Insights and Settings support

--- src/aks-demo.StateService/Startup.cs
+++ src/aks-demo.StateService/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using aks_demo.Shared;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,9 @@ namespace aks_demo.StateService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.Configure<AppSettings>(Configuration.GetSection("Values"));
+            services.AddApplicationInsightsTelemetry();
+            services.AddApplicationInsightsTelemetry();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
